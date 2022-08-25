9e14cd2ca2f301c331985db70bfd4e308f607af7
101beardo <101tash101@gmail.com>
2022-08-26 01:29:49 +0530

    day 3 tasks completed

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -1,1 +1,8 @@
-
+let g_btn = document.getElementById("google_btn");
+g_btn.addEventListener("click", function () {
+  window.location = "https://app.greenhouse.io/users/auth/google_oauth2";
+});
+function storeData() {
+  let email = document.getElementById("email_input").value;
+  console.log(email);
+}
