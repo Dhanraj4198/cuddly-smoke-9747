9b5b394f6821d830bfe90fa52de3e146c4da686e
Dhanraj Jagtap <107112212+Dhanraj4198@users.noreply.github.com>
2022-08-26 20:52:31 +0530

    fw20_1048 day 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
-document.querySelector('#signInBtn').addEventListener('click',goToSignin)
-function goToSignin(){
-    console.log(event)
-  window.localStorage.href = 'sign_in.html'
+window.addEventListener("scroll", reveal);
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 150;
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("activate");
+    } else {
+      reveals[i].classList.remove("activate");
+    }
+  }
 }
-goToSignin()
