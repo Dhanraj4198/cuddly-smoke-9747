b85dee5eb374301af8cf3357ab564e895dea310f
101beardo <101tash101@gmail.com>
2022-08-27 07:51:39 +0530

    day4

--- scripts/navbarshow.js
+++ scripts/navbarshow.js
@@ -1,25 +1,28 @@
 document.addEventListener("click", (e) => {
   const isDropdownButton = e.target.matches("[data-dropdown_menu-button]");
-  if (!isDropdownButton && e.target.closest("[data-dropdown_menu]") != null) return;
+  if (!isDropdownButton && e.target.closest("[data-dropdown_menu]") != null)
+    return;
   let currentDropdown;
   if (isDropdownButton) {
     currentDropdown = e.target.closest("[data-dropdown_menu]");
     currentDropdown.classList.toggle("active");
   }
-  document.querySelectorAll("[data-dropdown_menu].active").forEach((dropdown) => {
-    if (dropdown === currentDropdown) return;
-    dropdown.classList.remove("active");
-  });
+  document
+    .querySelectorAll("[data-dropdown_menu].active")
+    .forEach((dropdown) => {
+      if (dropdown === currentDropdown) return;
+      dropdown.classList.remove("active");
+    });
 });
 
-document.querySelector('#signInBtn').addEventListener('click',goToSignin)
-function goToSignin(){
-  
-  window.location.href = 'sign_in.html'
+document.querySelector("#signInBtn").addEventListener("click", goToSignin);
+function goToSignin() {
+  console.log(event);
+
+  window.location.href = "sign_in.html";
 }
 
-document.querySelector('#logo').addEventListener('click',goToHome)
-function goToHome(){
-
-window.location.href = 'index.html'
+document.querySelector("#logo").addEventListener("click", goToHome);
+function goToHome() {
+  window.location.href = "index.html";
 }
